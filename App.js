b0c0edc4a90e5d3c9ff1cfc25fd192cc97a155c9
MatheusRocha12 <112430333+MatheusRocha12@users.noreply.github.com>
2024-02-19 17:17:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import{exibirHorario} from './components/Horario.jsx';
+import{fazerLogin} from './components/Login';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="lnk"
+        >
+          Learning React
+        </a>
+        <br />
+      <input type='text' id='txtLogin'/>
+      <br />
+      <input type='text' id='txtSenha'/>
+      <br />
+      <button
+        onClick={fazerLogin}
+        >Fazer Login </button>
+          <br />
+        <button
+        onClick={exibirHorario}
+        >Ver Horário </button>
+      </header>
+    </div>
+  );
+}
+
+exibirHorario();
+
+export default App;
